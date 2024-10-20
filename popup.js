c025f795fa6368c20551ec53530481cd5c41352a
stephangreiner <49695489+stephangreiner@users.noreply.github.com>
2024-10-20 18:31:42 +0200

    Add files via upload

--- popup.js
+++ popup.js
@@ -15,21 +15,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   function updateBlockedList() {
     chrome.storage.sync.get("blockedSites", function (data) {
-      var blockedSites = data.blockedSites;
-      if (!blockedSites) {
-        blockedSites = [];
-      }
+      var blockedSites = data.blockedSites || [];
       var blockedList = document.getElementById("blockedList");
       blockedList.innerHTML = "";
       blockedSites.forEach(function (site) {
         var li = document.createElement("li");
         li.textContent = site;
 
-        // add a remove button
-
+        // Add remove button
         var removeButton = document.createElement("button");
-        removeButton.textContent = "Remove";
-        removeButton.id = "removeSite";
+        removeButton.textContent = "x";
+        removeButton.id = "b_x";
         removeButton.addEventListener("click", function () {
           chrome.storage.sync.get("blockedSites", function (data) {
             var blockedSites = data.blockedSites;
@@ -42,7 +38,32 @@ document.addEventListener("DOMContentLoaded", function () {
           });
         });
 
+        // Add suspend button
+        var suspendButton = document.createElement("button");
+        suspendButton.textContent = "20 s";
+        suspendButton.id = "b_frei";
+        suspendButton.addEventListener("click", function () {
+          chrome.storage.sync.get("blockedSites", function (data) {
+            var blockedSites = data.blockedSites;
+            var index = blockedSites.indexOf(site);
+            if (index !== -1) {
+              // Temporarily remove the site
+              blockedSites.splice(index, 1);
+              chrome.storage.sync.set({ blockedSites: blockedSites });
+              updateBlockedList();
+
+              // After 1 minute, add the site back
+              setTimeout(function () {
+                blockedSites.push(site);
+                chrome.storage.sync.set({ blockedSites: blockedSites });
+                updateBlockedList();
+              }, 10000); // 10000 milliseconds = 20 s
+            }
+          });
+        });
+
         li.appendChild(removeButton);
+        li.appendChild(suspendButton);
 
         blockedList.appendChild(li);
       });
